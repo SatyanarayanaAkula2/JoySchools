d7faced6e6d55abcd85c071f5393680634fd2649
Competitivecodelearner <akulasatish49@gmail.com>
2026-08-19 11:35:16 +0530

    Backend updates completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,9 @@
+import "./config/env.js";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-
-// Load configurations
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 import { dbConnect } from "./config/db.js";
 import authRoutes from "./modules/auth/auth.routes.js";
